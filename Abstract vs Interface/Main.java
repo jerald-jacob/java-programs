84c858bde92cc265f6c60623806056ed47b25989
Jerald Jacob <jeraldjacob10@gmail.com>
2019-08-01 17:47:01 +0530

    What will be the out put

---
+++ Abstract vs Interface/Main.java
@@ -0,0 +1,64 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+SecondClass aa=new SecondClass();
+        aa.f1();
+    }
+
+}
+
+interface firstInterface {
+
+    void f1();
+
+    void f2();
+
+}
+
+interface secondInterface {
+
+    void f1();
+
+    void f2();
+
+}
+
+class FirstClass implements firstInterface, secondInterface {
+
+  
+    @Override
+    public void f2() {
+
+    }
+
+    @Override
+    public void f1() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
+abstract class firstAbstract{
+    abstract void f1();
+    void fun2(){
+        System.out.println("its a methord");
+}
+    
+}
+class SecondClass extends firstAbstract implements firstInterface{
+
+    @Override
+    public void f1() {
+        System.out.println("abstract class methord");
+    }
+   @Override
+    public void f1() {
+        System.out.println("abstract class methord");
+    }
+   
+    @Override
+    public void f2() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
